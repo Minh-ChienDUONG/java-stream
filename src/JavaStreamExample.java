b3b9cf933fa0c197dab46d23d675591128d91728
Chien DUONG <chienduong90@gmail.com>
2021-02-09 20:56:43 +0100

    demo java stream

---
+++ src/JavaStreamExample.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Product{
+    int id;
+    String name;
+    float price;
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public float getPrice() {
+        return price;
+    }
+}
+
+public class JavaStreamExample {
+    public static void main(String[] args) {
+        List<Product> productsList = new ArrayList<Product>();
+
+        productsList.add(new Product(1,"HP Laptop",25000f));
+        productsList.add(new Product(2,"Dell Laptop",30000f));
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));
+        productsList.add(new Product(4,"Sony Laptop",28000f));
+        productsList.add(new Product(5,"Apple Laptop",90000f));
+        //WITHOUT USING STREAM
+        List<Float> productPriceList = new ArrayList<Float>();
+        for(Product product: productsList){
+            if(product.price<30000){
+                productPriceList.add(product.price);
+            }
+        }
+        System.out.println(productPriceList);
+    }
+}
---
+++ src/JavaStreamItering.java
@@ -0,0 +1,11 @@
+import java.util.stream.Stream;
+
+public class JavaStreamItering {
+    public static void main(String[] args) {
+
+        Stream.iterate(1, element -> element + 1)
+                .filter(element -> element%5==0)
+                .limit(5)
+                .forEach(System.out::println);
+    }
+}
